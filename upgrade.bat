f9c40cac7d8da72e137921ef16a63a83c31af93f
H2Sxxa <h2sxxa0w0@gmail.com>
2023-12-05 07:25:41 +0800

    ✨ add bat to install project

---
+++ upgrade.bat
@@ -0,0 +1,3 @@
+@echo off
+.venv\Scripts\python.exe -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+.venv\Scripts\pip install --upgrade mkdocs-material -i https://pypi.tuna.tsinghua.edu.cn/simple
