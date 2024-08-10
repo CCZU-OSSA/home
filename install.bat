8c4fe7c8857642fbbab67fecc8a70815c351daed
H2Sxxa <h2sxxa0w0@gmail.com>
2024-08-10 10:35:05 +0800

    ✨ Upgrade BuildScript, now 2024😎

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
-python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
+python -m pip install -i https://mirror.sjtu.edu.cn/pypi/web/simple --upgrade pip
 echo create venv...
 python -m venv .venv
 echo success
-.venv\Scripts\pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade -r ./requirements.txt
+.venv\Scripts\pip install -i https://mirror.sjtu.edu.cn/pypi/web/simple --upgrade -r ./requirements.txt
 serve.bat
--- upgrade.bat
+++ upgrade.bat
@@ -1,3 +1,3 @@
 @echo off
-.venv\Scripts\python.exe -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-.venv\Scripts\pip install --upgrade -r ./requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+.venv\Scripts\python.exe -m pip install --upgrade pip -i https://mirror.sjtu.edu.cn/pypi/web/simple
+.venv\Scripts\pip install --upgrade -r ./requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple
